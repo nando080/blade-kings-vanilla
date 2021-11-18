8c3e720643d6e99f7e43c6a2ebb773a560b80cca
nando080 <nando080@hotmail.com>
2021-11-18 07:08:21 -0300

    image gallery remake gallery html css #1

--- js/script.js
+++ js/script.js
@@ -34,67 +34,6 @@ const changeSlide = () => {
     slidesEl[currentSlideIndex - 1].classList.add('is-active')
 }
 
-const getNumberOfImagesPerGallery = size => {
-    if (size >= 1140) {
-        return 12
-    }else if (size >= 820) {
-        return 6
-    } else {
-        return 4
-    }
-}
-
-const getImageGalleryContainerSize = () => galleryContainerEl.getBoundingClientRect().width
-
-const getCurrentNumberOfGalleries = () => 
-    totalImagesInGalleries / getNumberOfImagesPerGallery(getImageGalleryContainerSize())
-
-const buildImageGalleries = () => {
-    let currentImage = 1
-
-    const images = []
-
-    for (let i = 0; i < getCurrentNumberOfGalleries(); i++) {
-        let imageString = ''
-        for (let j = 0; j < getNumberOfImagesPerGallery(getImageGalleryContainerSize()); j++) {
-            const currentIndex = currentImage >= 10 ? currentImage : `0${currentImage}`
-            imageString += `
-            <img class="l-gallery__image" src="img/gallery/img-${currentIndex}-thumb.jpg" data-index="${currentImage}">
-            `
-            currentImage++
-        }
-        images.push(imageString)
-    }
-
-    const galleriesString = images.reduce((accumulator, image) => {
-        return accumulator + `
-        <div class="l-gallery__image-gallery">${image}</div>
-        `
-    }, '')
-
-    return galleriesString
-}
-
-const insertGalleriesIntoDOM = () => {
-    galleryContainerEl.innerHTML = buildImageGalleries()
-}
-
-const createIndexButton = index => {
-    const indexBtn = document.createElement("div")
-    indexBtn.classList.add('l-gallery__button')
-    indexBtn.dataset.index = index
-    return indexBtn
-}
-
-const fillIndexButtonContainer = () => {
-    indexButtonsContainerEl.innerHTML = ''
-    for (let i = 0; i < getCurrentNumberOfGalleries(); i++) {
-        indexButtonsContainerEl.appendChild(createIndexButton(i + 1))
-    }
-}
-
-console.log(createIndexButton(1))
-
 let changeSlideInterval = setInterval(() => {
     incrementSlideIndex()
     changeSlide()
@@ -129,11 +68,3 @@ navLinksEl.forEach(link => {
     navBarEl.classList.remove('is-active')
     })
 })
-
-window.addEventListener('resize', () => {
-    insertGalleriesIntoDOM()
-    fillIndexButtonContainer()
-})
-
-insertGalleriesIntoDOM()
-fillIndexButtonContainer()
